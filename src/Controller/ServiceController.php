e63f041a7a0d47bf25a819c018332cf1bac88d76
Artem <artem.kyiv@outlook.com>
2020-04-23 22:37:51 +0300

    feat: monobank rates

--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -35,12 +35,23 @@ class ServiceController extends AbstractController
         $eur = $result[33];
         $usd = $result[26];
         $rates = ['EUR' => $eur['rate'], 'USD' => $usd['rate'], 'UAH' => 1];
+
         $response = $client->request('GET', 'http://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11');
         $result = $response->toArray();
         $privatRates['USD'] = round($result[0]['buy'], 2).' / '.round($result[0]['sale'], 2);
         $privatRates['EUR'] = round($result[1]['buy'], 2).' / '.round($result[1]['sale'], 2);
 
-        return ['services' => $repository->orderByNextBilling($this->getUser()), 'rates' => $rates, 'privatRates' => $privatRates];
+        $response = $client->request('GET', 'https://api.monobank.ua/bank/currency');
+        $result = $response->toArray();
+        $monoRates['USD'] = round($result[0]['rateBuy'], 2).' / '.round($result[0]['rateSell'], 2);
+        $monoRates['EUR'] = round($result[1]['rateBuy'], 2).' / '.round($result[1]['rateSell'], 2);
+
+        return [
+            'services' => $repository->orderByNextBilling($this->getUser()),
+            'rates' => $rates,
+            'privatRates' => $privatRates,
+            'monoRates' => $monoRates,
+        ];
     }
 
     /**
