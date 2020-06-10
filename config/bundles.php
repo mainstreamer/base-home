768b3a6f7da8b871477a5e45319567aa5c8efa1d
Artem <artem.kyiv@outlook.com>
2020-06-10 19:53:56 +0300

    feat: sentry added

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
     Omines\DataTablesBundle\DataTablesBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
 ];
